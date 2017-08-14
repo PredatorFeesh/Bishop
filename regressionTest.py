4e049fb3c6e45f669ef246fe74e243f01771b4ee
Jason Azayev <azayevjason@gmail.com>
2017-08-14 14:47:59 -0400

    Took out minERMS and fixed probability

--- regressionTest.py
+++ regressionTest.py
@@ -1,5 +1,5 @@
 from matplotlib import pyplot as plt
-from math import sin, pi
+from math import sin, pi, log
 from random import random
 from curveFitting.curve import genCoef, errorRMS, y
 from utils.gauss import gauss, mean
@@ -29,8 +29,8 @@ def Main():
     plt.xlabel("Input")
     plt.ylabel("Values")
     
-    xSin = []
-    ySin = []
+    xFunc = []
+    yFunc = []
 
     xMachine= []
     yMachine= []
@@ -45,14 +45,14 @@ def Main():
     end= (pi*2)
     
     #Change this function
-    func = lambda x : 10*sin(x)
+    func = lambda x : 1/(x+1)+x**2
 
-    while i <= end: # Plots the SIN curve
-        xSin.append(i)
-        ySin.append(func(i))
+    while i <= end: # Plots the func(x) curve
+        xFunc.append(i)
+        yFunc.append(func(i))
         i+=0.01
 
-    for g in frange(0.0,6.5, 0.05): # Random points along the curve generated
+    for g in frange(0.0,end, 0.05): # Random points along the curve generated
         rand = (-1)**int(random()*10) * (random()*2)
         xMachine.append(g) # stores randomness x
         yMachine.append(rand + func(g)) # stores the randomness y
@@ -68,17 +68,18 @@ def Main():
     M = 3
     genCoef(M,N, xMachine, yMachine)
 
-    for i in frange(0.0,xSin[-1], 0.01):
+    for i in frange(0.0,end, 0.01):
         xPredict.append(i)
         pred = y(M,i)
         yPredict.append( pred )
     #End order M geenrating
 
-    #This is for predicting the best order
-    plt.plot(xSin,ySin)
+    plt.plot(xFunc,yFunc)
     plt.plot(xPredict, yPredict)
     plt.scatter(xMachine,yMachine)
 
+    '''
+    #This is for predicting the best order
     miniOrd = -1
     miniErr = 10**15
     for M in range(0,N):
@@ -97,18 +98,17 @@ def Main():
         yMin.append(y(miniOrd, i))
     plt.plot(xMin, yMin)
     #END PLOT OF BEST PREDICTED WITH ERROR RMS
-        
+    '''
     
     #Probabiliy of the data
     plt.subplot(222)
     plt.title("Probability of finding a point at output Y")
     plt.xlabel("Y")
     plt.ylabel("Probability")
-    for ii in frange(-18.000, 18.000, 0.1):
+    for ii in frange(min(yMachine), max(yMachine), 0.1):
         yProb.append(ii)
         xProb.append( gauss(ii, yMachine) )
     plt.plot(xProb, yProb)
-    print(mean(yMachine))
     #End data probability
     
     
